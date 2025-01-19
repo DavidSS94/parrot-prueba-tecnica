3f2376d9c6da8032b688b9de606676501ce56fc5
Ubuntu <ubuntu@ip-172-31-95-248.ec2.internal>
2025-01-19 00:27:07 +0000

    feath(sql): Add get session function

--- src/tools/sql.py
+++ src/tools/sql.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy import URL, Connection, String, Integer
-from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 from src.constants import POSTGRESQL_HOST, POSTGRESQL_USER, POSTGRESQL_PASS, POSTGRESQL_PORT, POSTGRESQL_DB
 
@@ -13,7 +13,7 @@ password=POSTGRESQL_PASS
 port=POSTGRESQL_PORT
 database=POSTGRESQL_DB
 
-def connection() -> Connection:
+def get_engine() -> Connection:
     sql_url = URL.create(
         drivername,
         username,
@@ -22,14 +22,20 @@ def connection() -> Connection:
         port,
         database
     )
-    engine = create_engine(sql_url)
+    engine = create_engine(sql_url, echo=True)
     return engine
 
+
 def create_db() -> tuple[Integer, String]:
-    conn = connection()
+    conn = get_engine()
 
     if not database_exists(conn.url):
         create_database(conn.url)
         return 200, "Database created successfully"
     else:
         return 200, "Database already exist"
+
+
+def get_session(engine) -> String:
+    Session = sessionmaker(engine)
+    return Session()
