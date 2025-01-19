55394da7ef0cf12561921eb11cf02eb118b25a78
Ubuntu <ubuntu@ip-172-31-95-248.ec2.internal>
2025-01-19 05:47:17 +0000

    feath(waiter): Create CRUD process

--- src/models/waiter.py
+++ src/models/waiter.py
@@ -1,4 +1,9 @@
-from sqlalchemy import String
+from typing import Optional
+
+from flask import g
+
+from sqlalchemy import select
+from sqlalchemy import String, Tuple, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.base import Base
@@ -12,6 +17,111 @@ class Waiter(Base):
     )
     name: Mapped[str] = mapped_column(String, nullable=False)
 
-    def __self__(self):
-        return self.email
+    def __init__(
+            self,
+            body: Optional[dict] = {}
+        ):
 
+        if 'email' not in body or 'name' not in body:
+            return 500, "El waiter debe de tener Email o Name"
+
+        self.email = body['email']
+        self.name = body['name']
+
+
+    def create(self) -> tuple[Integer, String]:
+
+        try:
+            g.session.add(self)
+            g.session.commit()
+            return 200, "Mesero registrado"
+
+        except Exception as error:
+            print(error.args[0])
+            return 500, error.args[0]
+
+
+    def get_all(self):
+        data = []
+        try:
+            waiters = g.session.query(Waiter).all()
+
+            for waiter in waiters:
+                data.append({
+                    "email": waiter.email,
+                    "name": waiter.name
+                })
+            return 200, "", data
+
+        except Exception as error:
+            print(error.args[0])
+            return 500, error.args[0], data
+
+
+    def find_one(
+            self,
+            name: String
+        ):
+
+        data = {}
+
+        try:
+            waiter = g.session.query(Waiter).filter(
+                Waiter.name == name
+            ).first()
+
+            if hasattr(waiter, 'email'):
+                data['email'] = waiter.email
+                data['name'] = waiter.name
+                return 200, "", data
+
+            else:
+                return 500, "El mesero solicitado no fue encontrado", data
+
+        except Exception as error:
+            print(error.args[0])
+            return 500, error.args[0], data
+
+
+    def delete(
+            self,
+            name: String
+        ):
+
+        try:
+            pk = self.get_pk(name)
+
+            if not pk == None:
+                g.session.query(Waiter).filter(
+                    Waiter.email == pk
+                ).delete()
+                g.session.commit()
+                return 200, "Mesero borrado con exito"
+
+            return 500, "El mesero solicitado no fue encontrado"
+
+        except Exception as error:
+            print(error.args[0])
+            return 500, error.args[0]
+
+
+    def get_pk(
+            self,
+            name: String
+        ):
+
+        data = {}
+
+        try:
+            waiter = g.session.query(Waiter).filter(
+                Waiter.name == name
+            ).first()
+
+            if hasattr(waiter, 'email'):
+                return waiter.email
+
+            else:
+                return None
+
+        except Exception as error:
+            print(error.args[0])
